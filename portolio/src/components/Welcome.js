a516a550fee57f347a9837c1972f90aedf1d9baf
Tanner Leigh <tannerhleigh@gmail.com>
2020-11-17 17:06:43 -0800

    [css] add floating effect to welcome

--- portolio/src/components/Welcome.js
+++ portolio/src/components/Welcome.js
@@ -3,7 +3,11 @@ import React from 'react';
 const Welcome = (props) => {
     return (
         <div>
-            
+            <div className="welcomeContainer">
+                <div className="floating">
+                    <h2> Welcome </h2>
+                </div>
+            </div>
         </div>
     )
 }
