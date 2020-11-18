2d320d05a0f8393d2d1c7ffb825cd8a5e45dd29a
Tanner Leigh <tannerhleigh@gmail.com>
2020-11-17 16:51:17 -0800

    [css] add basic styling for main page

--- portolio/src/components/NavBar.js
+++ portolio/src/components/NavBar.js
@@ -1,33 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import me from '../assets/tanner2.png';
 
 const NavBar = (props) => {
     return (
+        <>
         <nav className="navbar navbar-expand-lg navbar-light bg-white">
             <div className="container">
-                <Link className="navbar-brand" to="/" > Tanner Leigh
-                </Link>
-                
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07" aria-controls="#navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-
-                <div className="collapse navbar-collapse" id="navbarsExample07">
-                    <ul className="navbar-nav ml-auto">
-                        {
-                            <>
-                                <li className="nav-item">
-                                    <Link className="nav-link text-info" to="/about">About</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link text-info" to="/Data">Work</Link>
-                                </li>
-                            </>
-                        }
-                    </ul>
-                </div>
+                <Link className="navbar-brand" to="/" > TL </Link>
             </div>
         </nav>
+        <div className="navbarContainer">
+            <div>
+                <img className="profilePic" src={me} alt="Me"></img>
+            </div>
+            <div className="navbarRight">
+                <Link className="nav-link text-dark" to="/work">Work</Link>
+                <Link className="nav-link text-dark" to="/About">About</Link>
+                <Link className="nav-link text-dark" to="/Resume">Resume</Link>
+                <Link className="nav-link text-dark" to="/about">About</Link>
+            </div>
+        </div>
+        </>
     );
 }
 
--- portolio/src/components/Welcome.js
+++ portolio/src/components/Welcome.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Welcome = (props) => {
     return (
-        <div></div>
+        <div>
+            
+        </div>
     )
 }
 
