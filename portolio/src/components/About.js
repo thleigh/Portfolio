94a38eccdde1186631deb92f781f1456a613983b
Tanner Leigh <tannerhleigh@gmail.com>
2020-11-18 16:46:39 -0800

    [css] add shadow box around about container

--- portolio/src/components/About.js
+++ portolio/src/components/About.js
@@ -5,15 +5,17 @@ import me from '../assets/tanner2.png';
 const Dbout = (props) => {
     return (
         <div>
-            <div className="about">
-                <div>
-                    <img className="profilePic" src={me} alt="Me"></img>
-                </div>
-                <div className="aboutContainer">
-                    <Link className="nav-link text-dark" to="/work">Work</Link>
-                    <Link className="nav-link text-dark" to="/About">About</Link>
-                    <Link className="nav-link text-dark" to="/Resume">Resume</Link>
-                    <Link className="nav-link text-dark" to="/about">About</Link>
+            <div className="aboutMain">
+                <div className="about">
+                    <div>
+                        <img className="profilePic" src={me} alt="Me"></img>
+                    </div>
+                    <div className="aboutContainer">
+                        <Link className="nav-link text-dark" to="/work">Work</Link>
+                        <Link className="nav-link text-dark" to="/About">About</Link>
+                        <Link className="nav-link text-dark" to="/Resume">Resume</Link>
+                        <Link className="nav-link text-dark" to="/about">About</Link>
+                    </div>
                 </div>
             </div>
         </div>
