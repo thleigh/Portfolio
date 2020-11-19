7cdba11f7fa28793204edc2cc66e823abacbb25a
Tanner Leigh <tannerhleigh@gmail.com>
2020-11-18 18:40:32 -0800

    [fullpage.js] add fullpage folder with independent style.css

---
+++ portolio/src/components/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contact = (props) => {
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    )
+}
+
+export default Contact
---
+++ portolio/src/fullpage/FullPage.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import ReactFullpage from '@fullpage/react-fullpage';
+import Welcome from '../components/Welcome';
+import About from '../components/About';
+import Contact from '../components/Contact'
+import './style.css';
+
+const SEL = 'custom-section';
+const SECTION_SEL = `.${SEL}`;
+
+class FullPage extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      fullpages: [
+        {
+          page: <Welcome user={props.user}/>
+          
+        },
+        {
+          page: <Resources />
+        },
+        {
+          page: <About />,
+        }
+      ],
+    };
+  }
+
+  render() {
+    const { fullpages } = this.state;
+
+    if (!fullpages.length) {
+      return null;
+    }
+
+    return (
+      <div className="App">
+        <ReactFullpage
+
+          // API KEY
+          licenseKey={'SCROLL_KEY'}
+          // the page names in the url set as #
+          anchors={['Welcome', 'Resources', 'About']}
+          sectionSelector={SECTION_SEL}
+          // navigation dots on the right
+          navigation
+          // Scroll speed
+          scrollingSpeed = {700}
+
+          render={comp => (
+            <ReactFullpage.Wrapper>
+              {fullpages.map(({page}) => (
+                <div className={SEL}>
+                  <div className="fullPageBody">{page}</div>
+                </div>
+              ))}
+            </ReactFullpage.Wrapper>
+          )}
+        />
+      </div>
+    );
+  }
+}
+
+export default FullPage
