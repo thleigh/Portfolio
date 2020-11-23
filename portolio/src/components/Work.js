a20b93a617b999edaf437aadfeb151a6c89c7584
Tanner Leigh <tannerhleigh@gmail.com>
2020-11-22 18:52:21 -0800

    [work.js] add rel no referrer to each anchor tag

--- portolio/src/components/Work.js
+++ portolio/src/components/Work.js
@@ -5,20 +5,20 @@ const Work = (props) => {
         <div>
             <div className="workGrid">   
                 <div className="box">
-                    <a href="https://affirmations-client.herokuapp.com/" target="_blank">Affirmations</a>
-                    <a href="https://github.com/thleigh/Affirmations-App" target="_blank">Github Repository</a>
+                    <a href="https://affirmations-client.herokuapp.com/" rel="noreferrer" target="_blank">Affirmations</a>
+                    <a href="https://github.com/thleigh/Affirmations-App" rel="noreferrer" target="_blank">Github Repository</a>
                 </div>
                 <div className="box">
-                    <a href="http://upnext-app.herokuapp.com/" target="_blank">Upnext</a>
-                    <a href="https://github.com/thleigh/upnext" target="_blank">Github Repository</a>
+                    <a href="http://upnext-app.herokuapp.com/" rel="noreferrer" target="_blank">Upnext</a>
+                    <a href="https://github.com/thleigh/upnext" rel="noreferrer" target="_blank">Github Repository</a>
                 </div>
                 <div className="box">
-                    <a href="https://foodfuse.herokuapp.com/" target="_blank">FoodFuse</a>
-                    <a href="https://github.com/thleigh/FoodFuse" target="_blank">Github Repository</a>
+                    <a href="https://foodfuse.herokuapp.com/" rel="noreferrer" target="_blank">FoodFuse</a>
+                    <a href="https://github.com/thleigh/FoodFuse" rel="noreferrer" target="_blank">Github Repository</a>
                 </div>
                 <div className="box">
                     <a href="" target="_blank">The Mcdonalds Project</a>
-                    <a href="https://github.com/thleigh/The_Big_Mac_Front_End" target="_blank">Github Repository</a>
+                    <a href="https://github.com/thleigh/The_Big_Mac_Front_End" rel="noreferrer" target="_blank">Github Repository</a>
                 </div>
             </div>
         </div>
