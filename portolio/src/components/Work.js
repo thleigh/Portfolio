b1bc4b8e7d3e3bcb8b82278b18b753b23bea01d4
Tanner Leigh <tannerhleigh@gmail.com>
2020-11-22 18:50:27 -0800

    [work.js] add grid to work.js

--- portolio/src/components/Work.js
+++ portolio/src/components/Work.js
@@ -3,18 +3,24 @@ import React from 'react';
 const Work = (props) => {
     return (
         <div>
-            <a href="https://affirmations-client.herokuapp.com/" target="_blank">Affirmations</a>
-            <a href="https://github.com/thleigh/Affirmations-App" target="_blank">Github Repository</a>
-
-            <a href="http://upnext-app.herokuapp.com/" target="_blank">Upnext</a>
-            <a href="https://github.com/thleigh/upnext" target="_blank">Github Repository</a>
-
-            <a href="https://foodfuse.herokuapp.com/" target="_blank">FoodFuse</a>
-            <a href="https://github.com/thleigh/FoodFuse" target="_blank">Github Repository</a>
-
-            <a href="" target="_blank">The Mcdonalds Project</a>
-            <a href="https://github.com/thleigh/The_Big_Mac_Front_End" target="_blank">Github Repository</a>
-
+            <div className="workGrid">   
+                <div className="box">
+                    <a href="https://affirmations-client.herokuapp.com/" target="_blank">Affirmations</a>
+                    <a href="https://github.com/thleigh/Affirmations-App" target="_blank">Github Repository</a>
+                </div>
+                <div className="box">
+                    <a href="http://upnext-app.herokuapp.com/" target="_blank">Upnext</a>
+                    <a href="https://github.com/thleigh/upnext" target="_blank">Github Repository</a>
+                </div>
+                <div className="box">
+                    <a href="https://foodfuse.herokuapp.com/" target="_blank">FoodFuse</a>
+                    <a href="https://github.com/thleigh/FoodFuse" target="_blank">Github Repository</a>
+                </div>
+                <div className="box">
+                    <a href="" target="_blank">The Mcdonalds Project</a>
+                    <a href="https://github.com/thleigh/The_Big_Mac_Front_End" target="_blank">Github Repository</a>
+                </div>
+            </div>
         </div>
     )
 }
