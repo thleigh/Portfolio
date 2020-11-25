2b66bd57de258e13a419a94de319e1077444342b
Tanner Leigh <tannerhleigh@gmail.com>
2020-11-24 16:34:05 -0800

    [work.js] add useStyles

--- portolio/src/components/Work.js
+++ portolio/src/components/Work.js
@@ -9,6 +9,15 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const Work = (props) => {
 
+    const useStyles = makeStyles((theme) => ({
+        popover: {
+          pointerEvents: 'none',
+        },
+        paper: {
+          padding: theme.spacing(1),
+        },
+    }));
+
     return (
         <div>
             <div className="workGrid">   
