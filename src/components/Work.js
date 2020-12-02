0b1300626b1e8bda231327890809d8d87d15ccd5
Tanner Leigh <tannerhleigh@gmail.com>
2020-12-01 21:22:29 -0800

    [work.js] remove links and add button

--- src/components/Work.js
+++ src/components/Work.js
@@ -6,6 +6,7 @@ import Foodfuse from '../assets/foodfuselogo.png'
 import Popover from '@material-ui/core/Popover';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
 const Work = (props) => {
 
@@ -36,8 +37,13 @@ const Work = (props) => {
                         <img className="workLogo" alt="affirmations" src={Affirmations} width="100px"/>
                     </p>
                     <div className="workLinks">
-                        <a href="https://affirmations-client.herokuapp.com/" rel="noreferrer" target="_blank">Affirmations </a>
-                        <a href="https://github.com/thleigh/Affirmations-App" rel="noreferrer" target="_blank"> Github</a>
+                    <div>
+                        <Button aria-describedby={id} variant="contained" color="primary" onClick={handleClick}>
+                            Open Popover
+                        </Button>
+                    </div>
+                        {/* <a href="https://affirmations-client.herokuapp.com/" rel="noreferrer" target="_blank">Affirmations </a>
+                        <a href="https://github.com/thleigh/Affirmations-App" rel="noreferrer" target="_blank"> Github</a> */}
                     </div>
                 </div>
                 <div className="box">
@@ -45,8 +51,8 @@ const Work = (props) => {
                         <img className="workLogo tbmplogo" alt="the big mac project" src={TBMP} width="90px"/>
                     </p>
                     <div className="workLinks">
-                        <a href="" target="_blank">TBMP </a>
-                        <a href="https://github.com/thleigh/The_Big_Mac_Front_End" rel="noreferrer" target="_blank"> Github</a>
+                        {/* <a href="" target="_blank">TBMP </a>
+                        <a href="https://github.com/thleigh/The_Big_Mac_Front_End" rel="noreferrer" target="_blank"> Github</a> */}
                     </div>
                 </div>
                 <div className="box">
@@ -54,8 +60,8 @@ const Work = (props) => {
                         <img className="workLogo upnextlogo" alt="upnext" src={Upnext} width="110px"/>
                     </p>
                     <div className="workLinks">
-                        <a href="http://upnext-app.herokuapp.com/" rel="noreferrer" target="_blank">Upnext </a>
-                        <a href="https://github.com/thleigh/upnext" rel="noreferrer" target="_blank"> Github</a>
+                        {/* <a href="http://upnext-app.herokuapp.com/" rel="noreferrer" target="_blank">Upnext </a>
+                        <a href="https://github.com/thleigh/upnext" rel="noreferrer" target="_blank"> Github</a> */}
                     </div>
                 </div>
                 <div className="box">
@@ -63,8 +69,8 @@ const Work = (props) => {
                         <img className="workLogo" alt="Foodfuse" src={Foodfuse} width="130px"/>
                     </p>
                     <div className="workLinks">
-                        <a href="https://foodfuse.herokuapp.com/" rel="noreferrer" target="_blank">FoodFuse </a>
-                        <a href="https://github.com/thleigh/FoodFuse" rel="noreferrer" target="_blank"> Github</a>
+                        {/* <a href="https://foodfuse.herokuapp.com/" rel="noreferrer" target="_blank">FoodFuse </a>
+                        <a href="https://github.com/thleigh/FoodFuse" rel="noreferrer" target="_blank"> Github</a> */}
                     </div>
                 </div>
             </div>
