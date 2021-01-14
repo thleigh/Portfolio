65c725b149f27b1d742d76f6dc1a02549e567374
Tanner Leigh <tannerhleigh@gmail.com>
2021-01-14 13:31:44 -0800

    [work2] add test code

--- src/components/work2.js
+++ src/components/work2.js
@@ -1,54 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactFullpage from '@fullpage/react-fullpage';
-import { BrowserRouter as Router } from 'react-router-dom'
-
-import Welcome from '../components/Welcome';
-import About from '../components/About';
-import Work from '../components/Work';
-
+// NOTE: if using fullpage extensions/plugins put them here and pass it as props
 const pluginWrapper = () => {
-    require('../statics/fullpage.continuousHorizontal.min.js');
-  };
-
-const Work2 = (props) => {
-
-    return (
-        <div>
-            <ReactFullpage
-                pluginWrapper = {pluginWrapper}
-
-                //fullpage options
-                licenseKey = {'SCROLL_KEY'}
-                scrollingSpeed = {1000} /* Options here */
-                continuousHorizontal = {true}  /* Because we are using the extension */
-                continuousHorizontalKey = {'HORIZONTAL_KEY'}
-                controlArrows = {true}
-
-                render={({ state, fullpageApi }) => {
-                return (
-                    <ReactFullpage.Wrapper>
-                        <div className="section">
-                        <Router>
-                            <Welcome fullpageApi={fullpageApi}/>
-                        </Router>
-                        </div>
-                        <div className="section">
-                        <Router>
-                            <About fullpageApi={fullpageApi}/>
-                        </Router>
-                        </div>
-                        <div className="section">
-                        <Router>
-                            <Work />
-                        </Router>
-                        </div>
-                    </ReactFullpage.Wrapper>
-                );
-                }}
-            />
-        </div>
-    )
-}
-
+  require('../statics/fullpage.continuousHorizontal.min');
+};
+const Work2 = () => (
+  <ReactFullpage
+    pluginWrapper = {pluginWrapper}
+    //fullpage options
+    licenseKey = {'SCROLL_KEY'}
+    scrollingSpeed = {1000} /* Options here */
+    continuousHorizontal = {true}  /* Because we are using the extension */
+    continuousHorizontalKey = {'HORIZONTAL_KEY'}
+    render={({ state, fullpageApi }) => {
+      return (
+        <ReactFullpage.Wrapper>
+          <div className="section">
+            <p>Section 1 (welcome to fullpage.js)</p>
+            <button onClick={() => fullpageApi.moveSectionDown()}>
+              Click me to move down
+            </button>
+          </div>
+          <div className="section">
+            <p>Section 2</p>
+          </div>
+        </ReactFullpage.Wrapper>
+      );
+    }}
+  />
+);
 export default Work2
