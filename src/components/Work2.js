311ab038e182c4b52240d8b711433776c0cf906c
Tanner Leigh <tannerhleigh@gmail.com>
2021-01-25 15:36:03 -0800

    [work2] add styled hover effect using emotion

--- src/components/Work2.js
+++ src/components/Work2.js
@@ -6,34 +6,58 @@ import Upnext from '../assets/upnextss.png';
 import Foodfuse from '../assets/foodfusess.png';
 import styled from "@emotion/styled/macro"
 
-class Work2 extends Component {
-  // state = {
-  //   items: [
-  //     {id: 1, title: 'item #1'},
-  //     {id: 2, title: 'item #2'},
-  //     {id: 3, title: 'item #3'},
-  //     {id: 4, title: 'item #4'},
-  //   ]
-  // }
+const Background = styled.div({
+  backgroundSize: "cover",
+  backgroundRepeat: "no-repeat",
+  color: "#FFF",
+  position: "relative",
+  width: "500px",
+  height: "350px",
+  cursor: "pointer",
+  backgroundImage: "url(../assets/affirmationsss.png)",
+});
 
-  // constructor(props) {
-  //   super(props);
-  //   this.handleMouseHover = this.handleMouseHover.bind(this);
-  //   this.state = {
-  //     isHovering: false,
-  //   };
-  // }
-  // handleMouseHover() {
-  //   this.setState(this.toggleHoverState);
-  // }
-  // toggleHoverState(state) {
-  //   return {
-  //     isHovering: !state.isHovering,
-  //   };
-  // }
+const DisplayOver = styled.div({
+  height: "100%",
+  left: "0",
+  position: "absolute",
+  top: "0",
+  width: "100%",
+  zIndex: 2,
+  transition: "background-color 350ms ease",
+  backgroundColor: "transparent",
+  padding: "20px 20px 0 20px",
+  boxSizing: "border-box",
+});
+
+const BigTitle = styled.h2({
+  textTransform: "uppercase",
+  fontFamily: "Helvetica",
+});
+
+const Hover = styled.div({
+  opacity: 0,
+  transition: "opacity 350ms ease",
+});
+
+const SubTitle = styled.h4({
+  fontFamily: "Helvetica",
+  transform: "translate3d(0,50px,0)",
+  transition: "transform 350ms ease",
+});
+const Paragraph = styled.p({
+  transform: "translate3d(0,50px,0)",
+  transition: "transform 350ms ease",
+});
+const CTA = styled.a({
+  position: "absolute",
+  bottom: "20px",
+  left: "20px",
+});
+
+class Work2 extends Component {
 
   render () {
-    // const { items } = this.state;
     return (
         <>
             <div>
@@ -41,7 +65,7 @@ class Work2 extends Component {
             </div>
             <div>
             <Carousel itemsToShow={1}>
-                <div>
+                {/* <div>
                     <div className="affirmationsParent">
                       <div className="affirmationsChild">
                         <h4>Affirmations</h4>
@@ -56,7 +80,25 @@ class Work2 extends Component {
                       </div>
                     </div>
                     <img className="screenshot" src={Affirmations} alt="Affirmations"></img>
-                </div>
+                </div> */}
+                <Background>
+                  <DisplayOver>
+                    <BigTitle>Really Cool Title!</BigTitle>
+                    <Hover>
+                      <SubTitle>Affirmations</SubTitle>
+                      <Paragraph>
+                          Affirmations is a website that provides its users with easily accessible suicide prevention resources and
+                          affirming quotes. Users are able to find local professional help and also have the opportunity to talk to 
+                          random volunteers. 
+                      </Paragraph>
+                      <CTA>
+                        <a href="https://affirmations-client.herokuapp.com/" rel="noreferrer" target="_blank">Affirmations Site</a>
+                        <br></br>
+                        <a href="https://github.com/thleigh/Affirmations-App" rel="noreferrer" target="_blank">Github</a>
+                      </CTA>
+                    </Hover>
+                  </DisplayOver>
+                </Background>
                 <div >
                     <img className="screenshot" src={Upnext} alt="Upnext"></img>
                 </div>
