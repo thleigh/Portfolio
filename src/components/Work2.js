c2b50ce41a42bb879e27666068a2b1273537c7ae
Tanner Leigh <tannerhleigh@gmail.com>
2021-01-25 16:03:11 -0800

    [work2] add updated slides for rest of projects

--- src/components/Work2.js
+++ src/components/Work2.js
@@ -1,9 +1,5 @@
 import React, { Component } from 'react';
 import Carousel from 'react-elastic-carousel';
-// import Affirmations from '../assets/affirmationsss.png';
-// import TBMP from '../assets/affirmationsss.png';
-import Upnext from '../assets/upnextss.png';
-import Foodfuse from '../assets/foodfusess.png';
 import styled from "@emotion/styled/macro"
 
 const DisplayOver = styled.div({
@@ -33,19 +29,81 @@ const SubTitle = styled.h4({
   // fontFamily: "Helvetica",
   transform: "translate3d(0,50px,0)",
   transition: "transform 350ms ease",
+  textAlign: "center",
 });
 
 const Paragraph = styled.p({
   transform: "translate3d(0,50px,0)",
   transition: "transform 350ms ease",
+  textAlign: "center",
 });
 
-const Background = styled.div({
+const TBMP = styled.div({
   backgroundSize: "cover",
   backgroundRepeat: "no-repeat",
   color: "#FFF",
   position: "relative",
-  width: "500px",
+  width: "600px",
+  height: "350px",
+  cursor: "pointer",
+  backgroundImage: "url(/tbmp.jpg)",
+  [`:hover ${DisplayOver}`]: {
+    backgroundColor: "rgba(0,0,0,.5)",
+  },
+  [`:hover ${SubTitle}, :hover ${Paragraph}`]: {
+    transform: "translate3d(0,0,0)",
+  },
+  [`:hover ${Hover}`]: {
+    opacity: 1,
+  },
+});
+
+const Upnext = styled.div({
+  backgroundSize: "cover",
+  backgroundRepeat: "no-repeat",
+  color: "#FFF",
+  position: "relative",
+  width: "600px",
+  height: "350px",
+  cursor: "pointer",
+  backgroundImage: "url(/upnext.jpg)",
+  [`:hover ${DisplayOver}`]: {
+    backgroundColor: "rgba(0,0,0,.5)",
+  },
+  [`:hover ${SubTitle}, :hover ${Paragraph}`]: {
+    transform: "translate3d(0,0,0)",
+  },
+  [`:hover ${Hover}`]: {
+    opacity: 1,
+  },
+});
+
+const FoodFuse = styled.div({
+  backgroundSize: "cover",
+  backgroundRepeat: "no-repeat",
+  color: "#FFF",
+  position: "relative",
+  width: "600px",
+  height: "350px",
+  cursor: "pointer",
+  backgroundImage: "url(/foodfuse.jpg)",
+  [`:hover ${DisplayOver}`]: {
+    backgroundColor: "rgba(0,0,0,.5)",
+  },
+  [`:hover ${SubTitle}, :hover ${Paragraph}`]: {
+    transform: "translate3d(0,0,0)",
+  },
+  [`:hover ${Hover}`]: {
+    opacity: 1,
+  },
+});
+
+const Affirmation = styled.div({
+  backgroundSize: "cover",
+  backgroundRepeat: "no-repeat",
+  color: "#FFF",
+  position: "relative",
+  width: "600px",
   height: "350px",
   cursor: "pointer",
   backgroundImage: "url(/bg.jpg)",
@@ -62,8 +120,9 @@ const Background = styled.div({
 
 const CTA = styled.a({
   position: "absolute",
-  bottom: "20px",
-  left: "20px",
+  bottom: "80px",
+  left: "40%",
+  textAlign: "center",
 });
 
 class Work2 extends Component {
@@ -76,7 +135,7 @@ class Work2 extends Component {
             </div>
             <div>
             <Carousel itemsToShow={1}>
-                <Background>
+                <Affirmation>
                   <DisplayOver>
                     {/* <BigTitle>Really Cool Title!</BigTitle> */}
                     <Hover>
@@ -93,16 +152,62 @@ class Work2 extends Component {
                       </CTA>
                     </Hover>
                   </DisplayOver>
-                </Background>
-                <div >
-                    <img className="screenshot" src={Upnext} alt="Upnext"></img>
-                </div>
-                <div>
-                    <img className="screenshot" src={Upnext} alt="TBMP"></img>
-                </div>
-                <div>
-                    <img className="screenshot" src={Foodfuse} alt="Foodfuse"></img>
-                </div>
+                </Affirmation>
+                <Upnext>
+                  <DisplayOver>
+                    {/* <BigTitle>Really Cool Title!</BigTitle> */}
+                    <Hover>
+                      <SubTitle>Upnext</SubTitle>
+                      <Paragraph>
+                        upnext is a website where users can find out about upcoming show releases, where to buy them, 
+                        and how to buy them. upnext users will be able to browse older generations of sneakers and 
+                        interact with other users through community pages that allow them to post pictures, comment, 
+                        like, and favorite.        
+                      </Paragraph>
+                      <CTA>
+                        <a href="http://upnext-app.herokuapp.com/" rel="noreferrer" target="_blank">Upnext Site</a>
+                        <br></br>
+                        <a href="https://github.com/thleigh/upnext" rel="noreferrer" target="_blank">Github</a>
+                      </CTA>
+                    </Hover>
+                  </DisplayOver>
+                </Upnext>
+                <TBMP>
+                  <DisplayOver>
+                    {/* <BigTitle>Really Cool Title!</BigTitle> */}
+                    <Hover>
+                      <SubTitle>The Big Mac Project</SubTitle>
+                      <Paragraph>
+                        TBMP, otherwise known as "The Big Mac Project", finds the price of every Mcdonald's Big Mac meal across the United States and compares them.
+                        The goal behind The Big Mac Project is to find the relationship between the cost of living between each city and 
+                        how that effects the price of common goods. 
+                      </Paragraph>
+                      <CTA>
+                        <a href="/" target="_blank">The Big Mac Project Site</a>
+                        <br></br>
+                        <a href="https://github.com/thleigh/The_Big_Mac_Front_End" rel="noreferrer" target="_blank"> Github</a>
+                      </CTA>
+                    </Hover>
+                  </DisplayOver>
+                </TBMP>
+                <FoodFuse>
+                  <DisplayOver>
+                    {/* <BigTitle>Really Cool Title!</BigTitle> */}
+                    <Hover>
+                      <SubTitle>FoodFuse</SubTitle>
+                      <Paragraph>
+                        FoodFuse searches and sorts through the 4 major food delivery apps by the user's location and 
+                        returns the restaurant's data. FoodFuse provides the users with a comparison of the delivery 
+                        fee's and delivery times between each service so that the user can make an educated choice on which app to use.
+                      </Paragraph>
+                      <CTA>
+                        <a href="https://foodfuse.herokuapp.com/" rel="noreferrer" target="_blank">FoodFuse Site</a>
+                        <br></br>
+                        <a href="https://github.com/thleigh/FoodFuse" rel="noreferrer" target="_blank"> Github</a>
+                      </CTA>
+                    </Hover>
+                  </DisplayOver>
+                </FoodFuse>
             </Carousel>
             </div>
         </>
